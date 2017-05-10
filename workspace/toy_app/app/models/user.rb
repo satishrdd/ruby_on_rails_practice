2098734d1b48cb9888751948d504c90729bc014b
Satish <satishrdd123@gmail.com>
2017-05-10 13:53:20 +0530

    signup and login done

--- workspace/toy_app/app/models/user.rb
+++ workspace/toy_app/app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
 	validates :name,presence:true,length: {maximum:50}
 	validates :email,presence:true,length: {maximum:250},format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},uniqueness: {case_sensitive: false}
 	has_secure_password
-	validates :password,presence: true,length :{minimum: 6}
+	validates :password,presence: true,length: {minimum: 6}
 end
--- workspace/toy_app/config/routes.rb
+++ workspace/toy_app/config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  get 'sessions/new'
+
   get 'users/new'
 
   resources :microposts
   resources :users
-  root "users#index"
+  root "static_pages#home"
+  get '/help' ,to: 'static_pages#help'
+  get '/about' ,to: 'static_pages#about'
+  get '/contact',to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login' ,to:'sessions#new'
+  post '/login' ,to:'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
