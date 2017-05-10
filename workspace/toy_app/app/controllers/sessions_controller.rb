f1f6cfa4c9f7b4bade4fa10d3e727ad2ab27371f
Satish <satishrdd123@gmail.com>
2017-05-10 15:58:02 +0530

    more feature added to login

--- workspace/toy_app/app/controllers/sessions_controller.rb
+++ workspace/toy_app/app/controllers/sessions_controller.rb
@@ -5,7 +5,11 @@ class SessionsController < ApplicationController
   	user= User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
   		log_in user
-  		remember user
+  		if params[:session][:remember_me]=='1'
+  			remember(user)
+  		else
+  			forget(user)
+  		end
   		redirect_to user
   	else
   		flash.now[:danger] = 'Invalid email/password combination'
@@ -13,7 +17,7 @@ class SessionsController < ApplicationController
   	end
   end
   def destroy
-  	log_out
+  	log_out if logged_in?
   	redirect_to root_url
   end
 end
