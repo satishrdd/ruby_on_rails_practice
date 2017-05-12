d6f962f0c580e8add5c4929870c18e412e3e3142
Satish <satishrdd123@gmail.com>
2017-05-12 15:45:28 +0530

    almost done

--- workspace/toy_app/app/controllers/application_controller.rb
+++ workspace/toy_app/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+   before_action :set_cache_headers
   include SessionsHelper
   def hello
   	render html: "hello,world"
@@ -13,4 +14,12 @@ class ApplicationController < ActionController::Base
         redirect_to login_url
       end
     end
+
+    def set_cache_headers
+    response.headers["Cache-Control"] = "no-cache, no-store"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
+
+
 end
--- workspace/toy_app/app/controllers/static_pages_controller.rb
+++ workspace/toy_app/app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
   	if logged_in?
   		@micropost = current_user.microposts.build
-  		@feed_items = current_user.feed.paginate(page: params[:page])
+  		@feed_items = current_user.feed.paginate(page: params[:page], :per_page =>10 ,:total_entries=> 100)
   	end 
   end
 
